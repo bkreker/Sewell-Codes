4e3973033a1339caf44d1fa5be079c4049135ef1
Josh <jdegraw42@gmail.com>
2016-10-14 15:47:13 -0600

    Adwords Query in Sewell Queries

---
+++ SQL Queries/AdWords Sales by date.sql
@@ -0,0 +1,22 @@
+SELECT 
+	datepart(Year, OrderDate) AS 'Year', 
+	datepart(Month, OrderDate) AS 'Month', 
+	datepart(Week, OrderDate) AS 'Week', 
+	--datepart(Day, OrderDate) AS 'Day', 
+	sum(TotalPrice) AS 'Revenue', 
+	sum(TotalMaterialCost) AS 'COGS', 
+	sum(TotalPrice)-avg(TotalMaterialCost) AS 'GP'
+FROM ProductSalesSince2008
+WHERE OrderDate BETWEEN dbo.DaysAgo(365) and dbo.Today()
+	AND GoogleAnalyticsSource LIKE '%Google%' 
+	AND GoogleAnalyticsMedium LIKE '%cpc%'
+GROUP BY 
+	datepart(Year, OrderDate), 
+	datepart(Month, OrderDate), 
+	datepart(week, OrderDate)
+	--,datepart(Day, OrderDate) 
+ORDER BY 
+	datepart(Year, OrderDate), 
+	datepart(Month, OrderDate), 
+	datepart(week, OrderDate)
+	--,datepart(Day, OrderDate) 
