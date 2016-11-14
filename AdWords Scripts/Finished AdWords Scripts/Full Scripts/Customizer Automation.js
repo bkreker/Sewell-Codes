2eee6e74be6ca628d4822ae1684cf9191fc7b9d8
Josh <jdegraw42@gmail.com>
2016-11-14 12:54:48 -0700

    Lots of updates, mainly to Helpers

--- AdWords Scripts/Finished AdWords Scripts/Full Scripts/Customizer Automation.js
+++ AdWords Scripts/Finished AdWords Scripts/Full Scripts/Customizer Automation.js
@@ -2,7 +2,7 @@
  * Customizer Automtion
  * Created By: Josh DeGraw
  ************************************/
-var SCRIPT_NAME = ['Ad', 'Customizer'];
+var REPORT_NAME = ['Ad', 'Customizer'];
 var PRICE_TEXT = ['itemprop="price">', '</div>']
 var TITLES;
 
@@ -31,7 +31,7 @@ var REGEX_PRICE_CODE = [/(itemprop="price">\$\d*\.)\d+(<\/div>)/g, /(class="pric
 var REGEX_URL = /(http)s?:\/\//g;
 
 function main() {
-    var date = _getDate();
+    var date = _getDateTime();
     var day = date.day;
     var time = date.time;
     EMAIL_LIST = [
@@ -39,7 +39,7 @@ function main() {
     ];
     getPrices();
     updateReport();
-    EmailResults();
+    EmailResults(REPORT_NAME);
 }
 
 function getFileFromDrive() {
@@ -231,76 +231,6 @@ function getPriceAndSku(adGroup, url) {
     return result;
 }
 
-function print(msg) {
-    Logger.log(msg);
-}
-
-// A helper function to make rounding a little easier
-function round(value) {
-    // var decimals = Math.pow(10,DECIMAL_PLACES);
-    // return Math.round(value*decimals)/decimals;
-    return Math.floor(value);
-}
-
-//Helper function to format todays date
-function _getDate() {
-    var today = new Date();
-    var timeZone = AdWordsApp.currentAccount().getTimeZone();
-    var dayFormat = "MM-dd-yyyy";
-    var day = Utilities.formatDate(today, timeZone, dayFormat);
-    var time = AM_PM(today);
-
-    var date = {
-        day: day,
-        time: time
-    };
-
-    return date;
-}
-
-function AM_PM(date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
-    return strTime;
-}
-
-function EmailResults() {
-    var date = _getDate().day;
-    var attachment = EMAIL_LIST.join();
-    var subject = 'AdWords Alert: ' + SCRIPT_NAME.join('_');
-
-    var signature = '\n\nThis report was created by an automatic script by Josh DeGraw. If there are any errors or questions about this report, please inform me as soon as possible.';
-    var message = emailMessage() + signature;
-    var file_name = SCRIPT_NAME.join('_') + '_' + date;
-    var To;
-    var isPreview = '';
-
-    if (AdWordsApp.getExecutionInfo().isPreview()) {
-        To = EMAILS[0]
-        isPreview = 'Preview; No changes actually made to AdWords account.\n';
-    } else {
-        To = EMAILS.join();
-    }
-
-    MailApp.sendEmail({
-        to: To,
-        subject: subject,
-        body: isPreview + message,
-        attachments: [{
-            fileName: file_name + '.csv',
-            mimeType: 'text/csv',
-            content: attachment
-        }]
-    });
-
-    print('Email sent to ' + To);
-
-}
 
 function emailMessage() {
     var date = _getDate();
@@ -310,3 +240,6 @@ function emailMessage() {
     return message;
 
 }
+
+//Minified Helper Functions:
+function _getDateTime(){try{var a=new Date,b=AdWordsApp.currentAccount().getTimeZone(),c="MM-dd-yyyy",d=Utilities.formatDate(a,b,c),e=AM_PM(a),f={day:d,time:e};return f}catch(a){throw error("_getDateTime()",a)}}function AM_PM(a){try{var b=a.getHours()+1,c=a.getMinutes(),d=b>=12?"pm":"am";b%=12,b=b?b:12,c=c<10?"0"+c:c;var e=b+":"+c+" "+d;return e}catch(b){throw error("AM_PM(date: "+a+")",b)}}function CustomDateRange(a,b,c){try{null!==a&&void 0!==a||(a=91),null!==b&&void 0!==b||(b=0),void 0!==c&&""!==c&&null!==c||(c="YYYYMMdd");var d=_daysAgo(a),e=_daysAgo(b),f=_daysAgo(a,c).toString(),g=_daysAgo(b,c).toString(),h=[d,e],i=f+","+g,j={fromStr:f,toStr:g,fromObj:d,toObj:e,dateObj:h,string:i};return j}catch(d){throw error("CustomDateRange(fromDaysAgo: "+a+", tillDate: "+b+", format: "+c+")",d)}}function _daysAgo(a,b){try{var c=new Date;c.setDate(c.getDate()-a);var d;if(void 0!=b&&""!=b&&null!=b){var e=AdWordsApp.currentAccount().getTimeZone();d=Utilities.formatDate(c,e,b)}else d={day:c.getDate(),month:c.getMonth(),year:c.getYear()};return d}catch(c){throw error("_daysAgo(num: "+a+", format: "+b+")",c)}}function _today(a){try{var d,b=new Date,c=AdWordsApp.currentAccount().getTimeZone();return d=void 0!=a&&""!=a&&null!=a?Utilities.formatDate(b,c,a):{day:b.getDate(),month:b.getMonth(),year:b.getYear(),time:b.getTime()}}catch(b){throw error("_today(format: "+a+")",b)}}function _getDateString(){try{var a=new Date,b=AdWordsApp.currentAccount().getTimeZone(),c="MM-dd-yyyy",d=Utilities.formatDate(a,b,c);return d}catch(a){throw error("_getDateString()",a)}}function _todayIsMonday(){try{var a=36e5,b=new Date,c=new Date(b.getTime()+a),e=(c.getTime(),c.getDay());return Logger.log("today: "+c+"\nday: "+e),1===e}catch(a){throw error("todayIsMonday",a)}}function _rolling13Week(a){try{void 0!==a&&""!==a&&null!==a||(a="YYYYMMdd");var b=CustomDateRange(98,8,a),c=CustomDateRange(91,1,a),d=b.string+" - "+c.string,e={from:b,to:c,string:d};return e}catch(b){throw error("Rolling13Week(format: "+a+")",b)}}function formatKeyword(a){try{return a=a.replace(/[^a-zA-Z0-9 ]/g,"")}catch(b){throw error("formatKeyword(keyword: "+a+")",b)}}function round(a){try{var b=Math.pow(10,DECIMAL_PLACES);return Math.round(a*b)/b}catch(b){throw error("round(value: "+a+")",b)}}function getStandardDev(a,b,c){try{var d=0;for(var e in a)d+=Math.pow(a[e].stats[c]-b,2);return 0==Math.sqrt(a.length-1)?0:round(Math.sqrt(d)/Math.sqrt(a.length-1))}catch(d){throw error("getStandardDev(entites: "+a+", mean: "+b+", stat_key: "+c+")",d)}}function getMean(a,b){try{var c=0;for(var d in a)c+=a[d].stats[b];return 0==a.length?0:round(c/a.length)}catch(c){throw error("getMean(entites: "+a+", stat_key: "+b+")",c)}}function createLabelIfNeeded(a){try{AdWordsApp.labels().withCondition("Name = '"+a+"'").get().hasNext()||AdWordsApp.createLabel(a)}catch(b){throw error("createLabelIfNeeded(name: "+a+")",b)}}function sendResultsViaEmail(a,b){try{var i,c=a.match(/\n/g).length-1,d=_getDateTime().day,e="AdWords Alert: "+SCRIPT_NAME.join(" ")+" "+_titleCase(b)+"s Report - "+day,f="\n\nThis report was created by an automatic script by Josh DeGraw. If there are any errors or questions about this report, please inform me as soon as possible.",g=emailMessage(c)+f,h=SCRIPT_NAME.join("_")+d,j="";0!=c&&(AdWordsApp.getExecutionInfo().isPreview()?(i=EMAILS[0],j="Preview; No changes actually made.\n"):i=EMAILS.join(),MailApp.sendEmail({to:i,subject:e,body:j+g,attachments:[Utilities.newBlob(a,"text/csv",h+d+".csv")]}),Logger.log("Email sent to: "+i))}catch(c){throw error("sendResultsViaEmail(report: "+a+", level: "+b+")",c)}}function _titleCase(a){try{return a.replace(/(?:^|\s)\S/g,function(a){return a.toUpperCase()})}catch(b){throw error("_titleCase(str: "+a+")",b)}}function EmailResults(a){try{var g,b="AdWords Alert: "+a.join(" "),c="\n\nThis report was created by an automatic script by Josh DeGraw. If there are any errors or questions about this report, please inform me as soon as possible.",d=emailMessage()+c,e=emailAttachment(),f=_getDateString()+"_"+a.join("_"),h="";AdWordsApp.getExecutionInfo().isPreview()?(g=EMAILS[0],h="Preview; No changes actually made.\n"):g=EMAILS.join(),""!=d&&MailApp.sendEmail({to:g,subject:b,body:d,attachments:[{fileName:f+".csv",mimeType:"text/csv",content:e}]}),Logger.log("Email sent to: "+g)}catch(b){throw error("EmailResults(ReportName: "+a+")",b)}}function info(a){Logger.log(a)}function print(a){Logger.log(a)}function error(a,b){var c="ERROR in "+a+": "+b;return Logger.log(c),c}function warn(a){Logger.log("WARNING: "+a)}function isNumber(a){try{return a.toString().match(/(\.*([0-9])*\,*[0-9]\.*)/g)||NaN===a}catch(b){throw error("isNumber(obj: "+a+")",b)}}function hasLabelAlready(a,b){try{return a.labels().withCondition("Name = '"+b+"'").get().hasNext()}catch(c){throw error("hasLabelAlready(entity: "+a+", label"+b+")",c)}}
